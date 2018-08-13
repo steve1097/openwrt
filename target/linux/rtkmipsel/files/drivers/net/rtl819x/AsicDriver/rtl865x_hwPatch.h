7a9eb426a3b9657079ec97dbef115b5a119a9a78
steve1097 <stevechang10000@yahoo.com.tw>
2018-08-13 15:29:45 +0800

    add support for Realtek rtl8197f
    Signed-off-by: steve1097 <stevechang10000@yahoo.com.tw>

---
+++ target/linux/rtkmipsel/files/drivers/net/rtl819x/AsicDriver/rtl865x_hwPatch.h
@@ -0,0 +1,29 @@
+/*
+ *
+ *	The header file collects the information about the IC's different Version/Sub-version.
+ *		=> It's used by DRIVER to determine how to implement features for different ICs.
+ *
+ */  
+    
+#ifndef RTL865x_hwPatch_H
+#define RTL865x_hwPatch_H
+    
+/* Chip Version */ 
+#define RTL865X_CHIP_VER_RTL865XB	0x01
+#define RTL865X_CHIP_VER_RTL865XC	0x02
+#define RTL865X_CHIP_VER_RTL8196B	0x03
+#define RTL865X_CHIP_VER_RTL8196C	0x04
+    
+/* Revision ID of each chip */ 
+#define RTL865X_CHIP_REV_A		0x00
+#define RTL865X_CHIP_REV_B		0x01
+#define RTL865X_CHIP_REV_C		0x02
+#define RTL865X_CHIP_REV_D		0x03
+#define RTL865X_CHIP_REV_E		0x04
+    
+#if 1
+#define RTL819X_TLU_CHECK		1
+#endif /*  */
+    
+#endif /*  */
+    
